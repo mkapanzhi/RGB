8ca809613f7506adb8b0ad8692f62aabea244db4
mkapanzhi <mak1917132@yandex.ru>
2022-08-10 22:49:06 +0300

    Save before delegat logic

--- RGB/RGB/FirstVC.swift
+++ RGB/RGB/FirstVC.swift
@@ -7,7 +7,10 @@
 
 import UIKit
 
+
+
 class FirstVC: UIViewController {
+    
     
     
     var mainColor = Color(red: 1, green: 1, blue: 1)
@@ -15,19 +18,18 @@ class FirstVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor(red: CGFloat(mainColor.red), green: CGFloat(mainColor.green), blue: CGFloat(mainColor.blue), alpha: 1)
     }
-    
 
     @IBAction func goToRGBVC() {
         performSegue(withIdentifier: "goToRGBVC", sender: mainColor)
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination as? SecondVC,
               let editColor = sender as? Color else { return }
         destinationVC.editColor = editColor
     }
-    
-
 }
+
+
--- RGB/RGB/SecondVC.swift
+++ RGB/RGB/SecondVC.swift
@@ -9,23 +9,27 @@ import UIKit
 
 class SecondVC: UIViewController {
     var editColor: Color?
+    var saveData: Color?
+    
     
     @IBOutlet var colorView: UIView!
-    @IBOutlet weak var redSlider: UISlider!
-    @IBOutlet weak var greenSlider: UISlider!
-    @IBOutlet weak var blueSlider: UISlider!
-    
-    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet weak var doneButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initialColors()
+        saveData = editColor!
     }
     
     @IBAction func sliderMove() {
         changeBackgroundColor()
     }
-    
+    @IBAction func sendDataOnFirstVC(_ sender: Any) {
+        
+    }
     
     func initialColors() {
         guard let red = editColor?.red,
@@ -35,14 +39,20 @@ class SecondVC: UIViewController {
         redSlider.value = red
         greenSlider.value = green
         blueSlider.value = blue
+        
     }
     
     func changeBackgroundColor() {
         colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
-        guard ((editColor?.red = redSlider.value) != nil),
-              ((editColor?.green = greenSlider.value) != nil),
-              ((editColor?.blue = blueSlider.value) != nil) else { return }
-        
+        guard (editColor?.red = redSlider.value) != nil,
+              (editColor?.green = greenSlider.value) != nil,
+              (editColor?.blue = blueSlider.value) != nil else { return }
+        if saveData != editColor {
+            doneButton.isEnabled = true
+        } else {
+            doneButton.isEnabled = false
+        }
     }
+    
     
 }
